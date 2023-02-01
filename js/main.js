566171e46771fac1f92b67a38ae33133f3618e4c
Amr-Elharery <amrelharery@gmail.com>
2023-02-01 14:29:19 +0200

    New Change

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let yearlyTargetSpans = document.querySelectorAll(".prog > span");
+let yearlyTargetProgSpans = document.querySelectorAll(".prog span span");
+
+window.onload = () => {
+  // Progress Fill
+  yearlyTargetSpans.forEach(e => {
+    e.style.transition = '1.5s ease-out'
+    e.style.width = `${e.dataset.progress}%`;
+  })
+
+  // Progress Counter
+  yearlyTargetProgSpans.forEach(e => {
+    // e.style.transition = '0.5s'
+    let n = 1
+    let int = setInterval(() => {
+      e.innerHTML = `${n}%`
+      if (n < e.parentElement.dataset.progress) {
+        n++;
+      } else {
+        clearInterval(int);
+      }
+    }, 20)
+  })
+
+}
